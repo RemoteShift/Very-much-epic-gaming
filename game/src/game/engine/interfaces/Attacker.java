0c0152caf25f8de98b315faa18d26ae3a3b7b5be
jojo252 <119111588+jojo252@users.noreply.github.com>
2024-03-02 16:09:56 +0200

    frameework

---
+++ game/src/game/engine/interfaces/Attacker.java
@@ -0,0 +1,13 @@
+package game.engine.interfaces;
+
+public interface Attacker {
+	public int getCurrentHealth() {
+		
+	}
+	public void setCurrentHealth(int health) {
+		
+	}
+	public int getResourcesValue() {
+		
+	}
+}
